eda24e9f653c16758fdad4f6d8461815a9b3d294
Robert Clausecker <fuzxxl@gmail.com>
2010-09-26 11:19:02 +0800

    Allgorithmus fertiggestellt; möglicherweise noch Fehlerbehaftet.

--- Aufgabe3/src/Main.hs
+++ Aufgabe3/src/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
@@ -19,13 +20,24 @@ module Main (
 import Aufgabe3.Algorithmen
 import Aufgabe3.IO
 import Text.Printf
+import System.Environment (getArgs)
+import Control.Monad (when)
 
 main :: IO ()
 main = do
   ab <- auftragsbuchAusStdin
+  args <- getArgs
+  when (length args < 2)
+    (fail "Es werden zwei Argumente benötigt.")
   putStrLn "Repräsentation der Eingabedaten:"
   print ab
   putStrLn "Anzahl der benötigten Fahrzeuge:"
   let (a,b,c) = benötigteFahrzeuge ab
-  printf "Lager A: %3d\nLager B: %3d\nLager C: %3d" a b c
-  printf "Insgesamt: %4d" (a + b + c)
+  printf "Lager A: %3d\nLager B: %3d\nLager C: %3d\n" a b c
+  printf "Insgesamt: %4d\n\n" (a + b + c)
+  putStrLn "Optimiere..."
+  let [s,i] = map read (take 2 args)
+      ab' = optimiereFahrten s i ab
+      (a',b',c') = benötigteFahrzeuge ab'
+  printf "Lager A: %3d\nLager B: %3d\nLager C: %3d\n" a' b' c'
+  printf "Insgesamt: %4d\n\n" (a' + b' + c')
