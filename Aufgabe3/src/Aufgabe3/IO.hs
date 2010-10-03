72256f43b492fe7219956d048e97a83c09178094
Robert Clausecker <fuzxxl@gmail.com>
2010-10-03 13:04:15 +0800

    Dokumentation fertig. Makefile verändert.

--- Aufgabe3/src/Aufgabe3/IO.hs
+++ Aufgabe3/src/Aufgabe3/IO.hs
@@ -7,7 +7,6 @@ module Aufgabe3.IO
 
 import Aufgabe3.Datatypes
 import qualified Data.ByteString.Lazy.Char8 as B
---import System.IO
 import Control.Monad (when)
 
 -- Wir brauchen die fail Funktion.
@@ -15,7 +14,8 @@ auftragsbuchAusByteString :: Monad m => B.ByteString -> m Auftragsbuch
 auftragsbuchAusByteString string = do
   let
     linedString     = B.lines string
-    withoutComments = filter (\line -> B.words line /= [] && B.head line /= '#') linedString
+    withoutComments = --Filtert leere Zeilen und Zeilen die mit "#" beginnen.
+      filter (\line -> B.words line /= [] && B.head line /= '#') linedString
   when (length withoutComments /= 6)
     (fail "Aufgabe3.IO: Ungültige Eingabe: Ungültige Anzahl an Tagen")
   tourSets <- tourSetAusByteString `mapM` withoutComments
@@ -24,22 +24,20 @@ auftragsbuchAusByteString string = do
 
 tourSetAusByteString :: Monad m => B.ByteString -> m TourSet
 tourSetAusByteString string = do
-  let wörter = B.words string
-  when (length wörter /= 6)
+  let woerter = B.words string
+  when (length woerter /= 6)
     (fail "Aufgabe3.IO.tourSetAusByteString: Ungültige Anzahl an Touren.")
   zahlen <- maybe (fail $ "Aufgabe3.IO.tourSetAusByteString: Eingabe " ++
     "konnte nicht als Zahl verarbeitet werden.")
-    (return . map fst) (mapM B.readInt wörter)
+    (return . map fst) (mapM B.readInt woerter)
   return $ tourSetAusListe zahlen
 
 auftragsbuchAusDatei :: FilePath -> IO Auftragsbuch
 auftragsbuchAusDatei fp = do
   file <- B.readFile fp
-  ab <- auftragsbuchAusByteString file
-  ab `seq` return ab
+  auftragsbuchAusByteString file
 
 auftragsbuchAusStdin :: IO Auftragsbuch
 auftragsbuchAusStdin = do
   file <- B.getContents
-  ab <- auftragsbuchAusByteString file
-  ab `seq` return ab
+  auftragsbuchAusByteString file
