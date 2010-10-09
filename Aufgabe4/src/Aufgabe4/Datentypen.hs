99fc1665fe68ba855b6f7ad9ad72a440740c440d
Robert Clausecker <fuzxxl@gmail.com>
2010-10-09 23:38:28 +0800

    An Aufgabe 4 gearbeitet.
    Das Modul Aufgabe4.Statistik enthält die Hauptlogik des Programms.

--- Aufgabe4/src/Aufgabe4/Datentypen.hs
+++ Aufgabe4/src/Aufgabe4/Datentypen.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE BangPatterns #-}
-module Aufgabe4.Datentypen (
+module Aufgabe4.Datentypen {- (
     Kartenspiel (..),
-    Wuerfel (..),
-    ZweiWuerfel
-  ) where
+    Karte (..),
+    Auswahl
+  ) -} where
 
 import Data.Bits ()
 
@@ -18,55 +18,54 @@ data Kartenspiel = Kartenspiel
   , karte7 :: !Bool
   , karte8 :: !Bool
   , karte9 :: !Bool
-  } deriving (Eq,Ord,Enum,Bounded,Show)
+  } deriving (Eq,Ord,Show)
 
--- Was heißt Bits? ... Genau!
-instance Bits Kartenspiel where
-  Kartenspiel (a1 a2 a3 a4 a5 a6 a7 a8 a9) .&.
-    Kartenspiel (b1 b2 b3 b4 b5 b6 b7 b8 b9) =
-      Kartenspiel (
-        (a1 && b1) (a2 && b2) (a3 && b3)
-        (a4 && b4) (a5 && b5) (a6 && b6)
-        (a7 && b7) (a8 && b8) (a9 && b9)
-      )
-  Kartenspiel (a1 a2 a3 a4 a5 a6 a7 a8 a9) .|.
-    Kartenspiel (b1 b2 b3 b4 b5 b6 b7 b8 b9) =
-      Kartenspiel (
-        (a1 || b1) (a2 || b2) (a3 || b3)
-        (a4 || b4) (a5 || b5) (a6 || b6)
-        (a7 || b7) (a8 || b8) (a9 || b9)
-      )
-  -- TODO: Boilerplate beenden
 
--- Um die Typsicherheit zu erhöhen, wird ein Würfel als Aufzählung dargestellt.
+data Karte
+  = Karte1
+  | Karte2
+  | Karte3
+  | Karte4
+  | Karte5
+  | Karte6
+  | Karte7
+  | Karte8
+  | Karte9
+  deriving (Show,Ord,Eq,Enum,Bounded)
 
-data Wuerfel = Auge1
-             | Auge2
-             | Auge3
-             | Auge4
-             | Auge5
-             | Auge6
-             deriving(Show,Bounded,Eq,Ord)
+-- Möglichkeit, eine Karte auszuwählen.
+type Auswahl = Either Karte (Karte,Karte)
 
--- Wir hätten gerne Auge1 = 1, also leiten wir Enum selbst ab.
-instance Enum Wuerfel where
-  toEnum 1 = Auge1
-  toEnum 2 = Auge2
-  toEnum 3 = Auge3
-  toEnum 4 = Auge4
-  toEnum 5 = Auge5
-  toEnum 6 = Auge6
-  toEnum _ = error "Enum.toEnum.Wuerfel: Ungültiges Argument"
-  fromEnum Auge1 = 1
-  fromEnum Auge2 = 2
-  fromEnum Auge3 = 3
-  fromEnum Auge4 = 4
-  fromEnum Auge5 = 5
-  fromEnum Auge6 = 6
-  -- den Rest macht die Defaultimplentierung für uns.
+-- Funktionen, um obrige Datentypen zu manipulieren.
 
--- Wir Repräsentieren ein paar Augen als Tupel
+-- existiert die Karte im Kartenspiel?
+haveKarte :: Karte -> Kartenspiel -> Bool
+haveKarte = ([karte1,karte2,karte3,karte4,karte5,karte6,karte7,karte8,karte9]
+  !!) . fromEnum -- Funktion nimmt die Karte und liefert den Accessor zurück.
 
-type ZweiWuerfel = (Wuerfel,Wuerfel)
+-- Setzt den Wert einer beliebigen Karte
+changeKarte :: Karte -> Bool -> Kartenspiel -> Kartenspiel
+changeKarte karte value spiel = sets !! (fromEnum karte) where
+  sets =
+    [ spiel { karte1  = value}
+    , spiel { karte2  = value}
+    , spiel { karte3  = value}
+    , spiel { karte4  = value}
+    , spiel { karte5  = value}
+    , spiel { karte6  = value}
+    , spiel { karte7  = value}
+    , spiel { karte8  = value}
+    , spiel { karte9  = value}
+    ]
 
---
+addKarte, removeKarte :: Karte -> Kartenspiel -> Kartenspiel
+addKarte    = (flip changeKarte) True
+removeKarte = (flip changeKarte) False
+
+auswahlAnwendbar :: Auswahl -> Kartenspiel -> Bool
+auswahlAnwendbar (Left karte)  spiel = haveKarte karte spiel
+auswahlAnwendbar (Right (a,b)) spiel = haveKarte a spiel && haveKarte b spiel
+
+wendeAuswahlAn :: Auswahl -> Kartenspiel -> Kartenspiel
+wendeAuswahlAn (Left  karte) = removeKarte karte
+wendeAuswahlAn (Right (a,b)) = removeKarte b . removeKarte a
---
+++ Aufgabe4/src/Aufgabe4/Statistik.hs
@@ -0,0 +1,94 @@
+-- Dieses Modul enthält den statistischen Kram.
+module Aufgabe4.Statistik {- (
+  wahrscheinlichkeitAugen,
+  kombinationen
+) -} where
+
+import Aufgabe4.Datentypen
+
+-- Liste der Wahrscheinlichkeiten der Augen zweier Würfel
+{-# SPECIALIZE wahrscheinlichkeitAugen :: [Rational] #-}
+wahrscheinlichkeitAugen :: Fractional t => [t]
+wahrscheinlichkeitAugen =
+  [ 1 / 36 --  2
+  , 1 / 18 --  3
+  , 1 / 12 --  4
+  , 1 /  9 --  5
+  , 5 / 36 --  6
+  , 1 /  6 --  7
+  , 5 / 36 --  8
+  , 1 /  9 --  9
+  , 1 / 12 -- 10
+  , 1 / 18 -- 11
+  , 1 / 36 -- 12
+  ]
+
+-- Liste alle Möglichkeiten, eine Augenzahl durch ein bis zwei Karten abzudecken
+kombinationen :: [[Auswahl]]
+kombinationen =
+  [  --  2
+    [ Left   Karte2
+  ], --  3
+    [ Right (Karte1,Karte2)
+    , Left   Karte3
+  ], --  4
+    [ Right (Karte1,Karte3)
+    , Left   Karte4
+  ], --  5
+    [ Right (Karte1,Karte4)
+    , Right (Karte2,Karte3)
+    , Left   Karte5
+  ], --  6
+    [ Right (Karte1,Karte5)
+    , Right (Karte2,Karte4)
+    , Left   Karte6
+  ], --  7
+    [ Right (Karte1,Karte6)
+    , Right (Karte2,Karte5)
+    , Right (Karte3,Karte4)
+    , Left   Karte7
+  ], --  8
+    [ Right (Karte1,Karte7)
+    , Right (Karte2,Karte6)
+    , Right (Karte3,Karte5)
+    , Left   Karte8
+  ], --  9
+    [ Right (Karte1,Karte8)
+    , Right (Karte2,Karte7)
+    , Right (Karte3,Karte6)
+    , Right (Karte4,Karte5)
+    , Left   Karte9
+  ], -- 10
+    [ Right (Karte1,Karte9)
+    , Right (Karte2,Karte8)
+    , Right (Karte3,Karte7)
+    , Right (Karte4,Karte6)
+  ], -- 11
+    [ Right (Karte2,Karte9)
+    , Right (Karte3,Karte8)
+    , Right (Karte4,Karte7)
+    , Right (Karte5,Karte6)
+  ], -- 12
+    [ Right (Karte3,Karte9)
+    , Right (Karte4,Karte8)
+    , Right (Karte5,Karte7)
+  ]]
+
+-- Berechnet die Anzahl der Punkte, die ein Kartenspiel bringt.
+punkte :: Kartenspiel -> Int
+punkte (Kartenspiel k1 k2 k3 k4 k5 k6 k7 k8 k9) = sum list where
+  list = map snd . filter (not . fst) $ zip [k1,k2,k3,k4,k5,k6,k7,k8,k9] [1..9]
+
+{-
+Die Nachfolgende Funktion ist die Kernfunktion des Programms.  Sie berechnet den
+zu erwartenden Durchschnittswert eines Kartenspiels.  Die Idee ist, dass für
+alle möglichen Augensummen ermittelt wird, welche Möglichkeiten es gibt, geei-
+gnete Karten auszuwählen.
+  Sollte es keine Möglichkeit geben, so wird die Summe aller Karten zurück gege-
+ben, ansonsten wird die Funktion rekursiv für alle entstehenden Kartenspiele
+aufgerufen.  Die so erhaltenen Ergebnisse werden anschließend mit der Wahr-
+scheinlichkeit, dass sie eintreten multipliziert, summiert und zurückgegeben.
+-}
+
+bewerteKartenspiel :: (Fractional a) => Kartenspiel -> a
+bewerteKartenspiel = undefined
